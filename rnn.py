e7b06104e8cece0f6ab6f970a1e860d534e6facb
nicubuliga <nicu.buliga2000@gmail.com>
2021-12-31 15:58:29 +0200

    x_train and y_train

--- rnn.py
+++ rnn.py
@@ -13,14 +13,37 @@ def read_data():
     test_data = np.asarray(rows[:31], dtype=object)
     train_data = np.asarray(rows[31:], dtype=object)
 
-    return test_data, train_data
+    return test_data[::-1], train_data[::-1]
 
 
 def filter_data(set):
-    return np.asarray([x[3] for x in set], dtype=object)
+    return np.asarray([[float(x[3])] for x in set], dtype=object)
+
+
+def normalize(set):
+    min_value = np.min(set)
+    max_value = np.max(set)
+
+    for row in set:
+        row[0] = (row[0] - min_value) / (max_value - min_value)
+
+    return set
+
+
+def train_model(train_set):
+    x_train = []
+    y_train = []
+
+    for i in range(60, len(train_set)):
+        x_train.append(train_set[i-60:i])
+        y_train.append(train_set[i])
 
 
 if __name__ == "__main__":
     test_set, train_set = read_data()
-    test_set = filter_data(test_set)
-    print(test_set)
+    scaled_test_set = normalize(filter_data(test_set))
+    scaled_train_set = normalize(filter_data(train_set))
+    # for x in train_set:
+    #     print(x[3])
+    # print(scaled_test_set)
+    train_model(scaled_train_set)
