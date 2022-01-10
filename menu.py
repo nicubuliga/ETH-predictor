e8ade73e540c336d3ca38f7e94a66dea397e551c
dorin2k19 <58854355+dorin2k19@users.noreply.github.com>
2022-01-10 23:14:07 +0200

    background

--- menu.py
+++ menu.py
@@ -1,3 +1,5 @@
+from PIL import ImageTk, Image
+
 import rnn
 import tkinter as tk
 from tkinter import ttk
@@ -16,35 +18,42 @@ def start_pressed(root, durate, dataset_type):
 def initialize_menu():
     root = tk.Tk()
     root.title('ETH-Predictor')
-    root.geometry('800x600')
-    root['bg'] = '#00ccff'
+    root.geometry('640x427')
+    root.config(bg='yellow')
+
+    img = ImageTk.PhotoImage(Image.open("back2.jpg"))
+
+    # Show image using label
+    background= tk.Label(root, image=img)
+    background.place(x=0, y=0)
+
     # label declaration
-    title_label = ttk.Label(root, text="ETH-Predictor", foreground="white", background='#00ccff',
+    title_label = ttk.Label(root, text="ETH-Predictor", foreground="black",background='white',
                             font=("Comic Sans MS", 30))
-    title_label.place(x=300, y=0)
+    title_label.place(x=150, y=0)
 
-    dataset_type_label = ttk.Label(root, text="Dataset type", foreground="white", background="#00ccff",
+    dataset_type_label = ttk.Label(root, text="Dataset type", foreground="black",background='white',
                                    font=("Comic Sans MS", 15))
-    dataset_type_label.place(x=300, y=150)
+    dataset_type_label.place(x=150, y=150)
 
-    durate_label = ttk.Label(root, text="Durate:", foreground="white", background='#00ccff',
+    durate_label = ttk.Label(root, text="Durate:", foreground="black",background="white",
                              font=("Comic Sans MS", 15))
-    durate_label.place(x=300, y=180)
+    durate_label.place(x=150, y=180)
 
     # comboBox
     dataset_type_combo = ttk.Combobox(root, width=10, font=("Comic Sans MS", 10))
     dataset_type_combo['values'] = ('daily', 'hourly', 'minute')
     dataset_type_combo.set('daily')
-    dataset_type_combo.place(x=460, y=158)
+    dataset_type_combo.place(x=310, y=158)
     dataset_type_combo.state(['readonly'])
 
     durate_spinbox = tk.Spinbox(root, from_=1, to=1000, width=5, font=("Comic Sans MS", 10),
                                 highlightbackground="white")
-    durate_spinbox.place(x=460, y=188)
+    durate_spinbox.place(x=310, y=188)
 
     start_button = tk.Button(root, text="Start", font=("Comic Sans MS", 20))
     start_button['command'] = lambda: start_pressed(root, durate_spinbox, dataset_type_combo)
-    start_button.place(x=390, y=320)
+    start_button.place(x=230, y=230)
 
     root.mainloop()
 
